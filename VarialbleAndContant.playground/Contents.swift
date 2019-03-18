010a0d8b18bdae23321b73e63c541f9b728dd464
kanyanat <kamyanat78@gmail.com>
2019-03-18 14:28:13 +0700

    Start datatype

---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปร
+
+var myName = "Kanyanat"
+myName = "Doramon"
+
+//Explicit Constant
+let myFriend = "Nopita"
+//myFriend = "Nopita" Error Because myFriend is let
+
+
